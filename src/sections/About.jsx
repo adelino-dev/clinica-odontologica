d3b0f353b4f031a8947c55163474c00b85dbaf25
adelino-dev <adelino.developer@gmail.com>
2026-08-12 14:48:44 -0300

    feat: implement about section with new layout, descriptive content, and team image

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,12 +1,23 @@
 import './About.css';
 
+import dentistTeamImg from "../assets/images/dentist-team.jpg";
+
 function About() {
     return (
-        <section id="about" className="about-section">
-            <div className="about-container">
-                <h2>Sobre Nós</h2>
-                <p>Bem-vindo à nossa clínica odontológica. Cuidamos do seu sorriso com profissionalismo e carinho.</p>
+        <section id="about-section">
+            <div id="about-text-container">
+                <h2 id="about-title">Quem somos?</h2>
+                <p id="about-text">
+                    Somos uma clínica odontológica especializada em cuidar da saúde e da beleza do seu sorriso.
+                    Sob a liderança da Dra. Fulana, clínica geral e especialista em Ortodontia, contamos com
+                    uma equipe de profissionais qualificados  para oferecer um atendimento completo e
+                    humanizado em diversas especialidades.
+                    <br /><br />
+                    Nosso compromisso é proporcionar uma experiência acolhedora, aliando ética, excelência
+                    e cuidado para que cada paciente possa sorrir com mais saúde e confiança.
+                </p>
             </div>
+            <img src={dentistTeamImg} alt="Imagem de fundo" />
         </section>
     );
 }
