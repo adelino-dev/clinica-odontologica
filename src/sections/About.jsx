2eda0b6e1ca184e674000f0373906b371acaf62d
adelino-dev <adelino.developer@gmail.com>
2026-08-14 10:44:36 -0300

    feat: add responsive layout and image toggle for About section on smaller screens

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -8,6 +8,7 @@ function About() {
             <div id="about-container">
                 <div id="about-text-container">
                     <h2 id="about-title">Quem somos?</h2>
+                    <img id="card-image-top" src={dentistTeamImg} alt="Imagem de fundo" />
                     <p id="about-text">
                         Somos uma clínica odontológica especializada em cuidar da saúde e da beleza do seu sorriso.
                         Sob a liderança da Dra. Fulana, clínica geral e especialista em Ortodontia, contamos com
@@ -18,7 +19,7 @@ function About() {
                         e cuidado para que cada paciente possa sorrir com mais saúde e confiança.
                     </p>
                 </div>
-                <img src={dentistTeamImg} alt="Imagem de fundo" />
+                <img id="card-image-left" src={dentistTeamImg} alt="Imagem de fundo" />
             </div>
         </section>
     );
